6361bba7923820b7b132ae21eb36c44271b47598
meteoryxx <258927593@qq.com>
2023-12-20 10:46:13 +0800

    Update app.js
    按fastgpt接口修改输入输出，加入log方便调试

--- app.js
+++ app.js
@@ -7,12 +7,22 @@ const app = new Koa();
 app.use(bodyParser());
 
 app.use(async ctx => {
-  // the parsed body will store in ctx.request.body
-  // if nothing was parsed, body will be an empty object {}
+    // the parsed body will store in ctx.request.body
+    // if nothing was parsed, body will be an empty object {}
 
-  let question = ctx.request.body.q;
-  let engine = ctx.request.body.engine ? ctx.request.body.engine : 'baidu';
-  ctx.body = { q: question, results: await searchEngineTool(question, engine) };
+    let question = ctx.request.body.searchKey;
+    let engine = ctx.request.body.engine ? ctx.request.body.engine : 'baidu';
+
+    const results = await searchEngineTool(searchKey, engine);
+    console.log('搜索结果:');
+    results.forEach(result => {
+        console.log('标题:', result.title);
+        console.log('链接:', result.href);
+        console.log('摘要:', result.abstract);
+        console.log('----------------------');
+    });
+    const output_result = results.map(result => result.title + result.abstract).join('\n');
+    ctx.body = { prompt: `Search key: ${searchKey}; Search results: ${output_result}` };
 });
 
 app.listen(3000);
